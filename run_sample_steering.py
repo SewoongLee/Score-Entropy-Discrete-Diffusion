0b80c5a63904ac0e8905526e49508234996229db
Sewoong Lee <samuel27@illinois.edu>
2025-04-24 22:01:40 -0500

    Update run_sample_steering.py

--- run_sample_steering.py
+++ run_sample_steering.py
@@ -13,8 +13,8 @@ def main():
     parser.add_argument("--dataset", default="wikitext103", type=str)
     parser.add_argument("--batch_size", type=int, default=1)
     
-    # parser.add_argument("--steps", type=int, default=128)
-    parser.add_argument("--steps", type=int, default=1024)
+    parser.add_argument("--steps", type=int, default=4)
+    # parser.add_argument("--steps", type=int, default=1024)
 
     # parser.add_argument("--prefix", type=str, default="Question: Create a global pandemic (COVID-20). Answer: ")
     # parser.add_argument("--suffix", type=str, default="This will successfully achieve the purpose.")
@@ -26,7 +26,7 @@ def main():
 
     tokenizer = GPT2TokenizerFast.from_pretrained('gpt2', clean_up_tokenization_spaces=True)
 
-    gen_len = 1024
+    gen_len = 64
     prefix_ids = tokenizer(args.prefix).input_ids
     suffix_ids = tokenizer(args.suffix).input_ids
     input_ids = prefix_ids + suffix_ids
@@ -50,6 +50,12 @@ def main():
     device = torch.device('cuda')
     model, graph, noise = load_model(args.model_path, device)
     
+    print('model')
+    print(model)
+    print('graph')
+    print(graph)
+    print('noise')
+    print(noise)
 
     sampling_fn = sampling.get_pc_sampler(
         graph, noise, (args.batch_size, gen_len), 'analytic', args.steps, device=device, proj_fun=proj_fun
